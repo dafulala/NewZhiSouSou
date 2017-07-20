326fcb291e17e23622895a0cec210df593e9b79a
dafulala <1426078490@qq.com>
2017-07-20 23:04:16 +0800

    杨宇昊js

--- ZhiSouSou/app/assets/javascripts/_xysj.js
+++ ZhiSouSou/app/assets/javascripts/_xysj.js
@@ -6,6 +6,24 @@ $(function (argument) {
           $('.y-xysj-contant-left-part > a').css('color','#337ab7')
         $(this).css('background','#4285f4')
          $(this).css('color','#fff')
+
+
 	});
 	
+         $('.y-form tbody tr td:first-child').on('mouseenter', function(event){
+       
+     
+        $(this).children('.font-table').show();
+	     });
+
+
+         $('.y-form tbody tr td:first-child').on('mouseleave', function(event){
+       
+     
+        $(this).children('.font-table').hide();
+	     });
+
+
 })
+
+
